338bba0f12215b86a34bbc8d106b63b96359a8e0
kumarnivi <nivethikashivakumar56@gmail.com>
2024-06-21 23:13:05 +0530

    impliment-emailjs

--- src/app/components/contact-popup/contact-popup.component.ts
+++ src/app/components/contact-popup/contact-popup.component.ts
@@ -81,7 +81,7 @@
 // }
 
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import emailjs from 'emailjs-com';
@@ -91,42 +91,58 @@ import emailjs from 'emailjs-com';
   templateUrl: './contact-popup.component.html',
   styleUrls: ['./contact-popup.component.css']
 })
-export class ContactPopupComponent {
+export class ContactPopupComponent implements OnInit  {
   emailjs: any;
-  myForm: FormGroup | any;
+  // myForm: FormGroup | any;
+
+
 
   constructor(private toastr: ToastrService, private fb: FormBuilder) {
-    this.initializeForm();
-  }
-
-  initializeForm(): void {
-    this.myForm = this.fb.group({
-      toName: new FormControl('', Validators.required),
-      fromEmail: new FormControl('', [Validators.required, Validators.email]),
-      phoneno: new FormControl('', Validators.required),
-      message: new FormControl('', Validators.required)
-    });
-  }
-
-  get f(): { [key: string]: AbstractControl } {
-    console.log(this.myForm)
-    return this.myForm.controls;
-  }
-  
-  ngOnInit(): void {
-    this.initializeForm();
     
+  }
+
+
+myForm = new FormGroup ({
+  toName :new FormControl(''),
+  fromName: new FormControl('',[Validators.required]),
+  fromEmail: new FormControl('', [Validators.required, Validators.email]),
+  phoneno: new FormControl('', [Validators.required]),
+  message: new FormControl('', [Validators.required])
+})
+
+get toName() {
+  return this.myForm.get('toName');
+}
+get fromName() {
+  return this.myForm.get('fromName');
+}
+
+get fromEmail() {
+  return this.myForm.get('fromEmail');
+}
+
+get phoneno() {
+  return this.myForm.get('phoneno');
+}
+
+get message() {
+  return this.myForm.get('message');
+}
+
+  ngOnInit(): void {
+ 
   }
 
   onSubmit() {
     if (this.myForm.valid) {
       const templateParams = {
-        to_name: this.myForm.value.toName,
+        to_name:this.myForm.value.toName,
+        from_name: this.myForm.value.fromName,
         from_email: this.myForm.value.fromEmail,
         phoneno: this.myForm.value.phoneno,
         message: this.myForm.value.message
       };
-console.log(this.myForm.value)
+
       emailjs.send('service_0q7yc3a', 'template_qm22pv4', templateParams, 'UXMQMKDA99YgnOre5')
         .then((response: { status: any; text: any; }) => {
           this.toastr.success('SUCCESS!');
@@ -139,4 +155,6 @@ console.log(this.myForm.value)
       this.toastr.error('Please fill all required fields.');
     }
   }
+
+
 }
